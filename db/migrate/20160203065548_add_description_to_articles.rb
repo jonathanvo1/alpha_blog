08026b6ce1bae6f6c8780e4ce9626327b4472a9a
Rails Udemy Course <jonathanvo1@gmail.com>
2016-02-10 07:20:32 +0000

    Create articles table, mdel, and add validations.

---
+++ db/migrate/20160203065548_add_description_to_articles.rb
@@ -0,0 +1,6 @@
+class AddDescriptionToArticles < ActiveRecord::Migration
+  def change
+    add_column :articles, :created_at, :datetime
+    add_column :articles, :updated_at, :datetime
+  end
+end
