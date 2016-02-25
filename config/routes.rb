1f1f8ee08d48efe810a5244c70e830c5626a4fd5
Rails Udemy Course <jonathanvo1@gmail.com>
2016-02-25 06:41:06 +0000

    sign up users from UI

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,11 @@ Rails.application.routes.draw do
 
   root 'pages#home'
   get 'about', to: 'pages#about'
-  
+
   resources :articles
+
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
