0d9c8dfd345a53b1d52d3f0c9c3047050a80d6c4
Rails Udemy Course <jonathanvo1@gmail.com>
2016-03-06 00:38:48 +0000

    complete authentication system

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,11 +5,21 @@ class SessionsController < ApplicationController
   end
 
   def create
-    render 'new'
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:success] = "You have successfully logged in"
+      redirect_to user_path(user)
+    else
+      flash.now[:danger] = "There was something wrong with your login information"
+      render 'new'
+    end
   end
 
   def destroy
-
+    session[:user_id] = nil
+    flash[:success ] = "You have logged out"
+    redirect_to root_path
   end
 
 end
